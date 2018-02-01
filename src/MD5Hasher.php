466a79639edbed711b1f51722115976a6a3352b6
MengFei <15552989278@qq.com>
2018-02-01 22:46:36 +0800

    init project

---
+++ src/MD5Hasher.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mengfei
+ * Date: 2018/2/1
+ * Time: 下午9:25
+ */
+
+namespace Laravist\Hasher;
+
+
+/**
+ * Class MD5Hasher
+ * @package Laravist\Hasher
+ */
+class MD5Hasher
+{
+    /**
+     * @param $value
+     * @param array $options
+     * @return string
+     */
+    public function make($value, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return hash('md5',$value . $salt);
+    }
+
+    /**
+     * @param $value
+     * @param $hashValue
+     * @param array $options
+     * @return bool
+     */
+    public function check($value, $hashValue, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return hash('md5',$value.$salt) === $hashValue;
+    }
+}
